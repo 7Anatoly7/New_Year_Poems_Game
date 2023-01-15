1f2f80f4f8691e75e00c9a0fe4d2119b91cefbc3
7Anatoly7 <122594490+7Anatoly7@users.noreply.github.com>
2023-01-15 13:50:54 +0300

    Update zelenkov_novogodniye_stishki_v3.10.py
    Схема программы игры

--- zelenkov_novogodniye_stishki_v3.10.py
+++ zelenkov_novogodniye_stishki_v3.10.py
@@ -1,16 +1,17 @@
 # Приветствие и описание игры
+print('Автор игры - Зеленков Анатолий')
 print('Приветствуем вас в Волшебном доме!')
 print('Здесь нужно рассказывать стихи новогодним персонажам, которые находятся в разных комнатах, и если им понравится, то вы получите от них подарок.')
 # Введите своё имя
 name = input('Как вас зовут? ')
 # Начало игры и проверка, хочет ли пользователь её начать
 begin = input('Отлично! Вы готовы начать игру? ')
-if begin == 'Да' or begin == 'да':
+if begin == 'Да' or begin == 'да' or begin == 'ДА':
     print('Отлично! Переместимся в дом!')
     while True:
         while True:
             # Выбор комнаты и персонажа, проверка на верный ввод
-            room = input('В какую комнату вы хотите войти? ')
+            room = input('Укажите номер комнаты, в которую вы хотите войти ')
             if room == '1':
                 character = 'Дед Мороз'
             elif room == '2':
@@ -21,7 +22,7 @@ if begin == 'Да' or begin == 'да':
             print(character, ': Привет, ', name, '!', sep='')
             # Проверка на то, хочет ли игрок вводить стих 
             question = input('Хотите рассказать стих? ')
-            if question == 'Да' or question == 'да':
+            if question == 'Да' or question == 'да' or question == 'ДА':
                 count = 0
                 # Рассказ стиха
                 line = input()
@@ -38,11 +39,11 @@ if begin == 'Да' or begin == 'да':
             break
         # Проверка на то, хочет ли пользователь продолжить игру
         question2 = input('Хотите продолжить? ')
-        while question2 != 'Да' and question2 != 'да' and question2 != 'Нет' and question2 != 'нет':
+        while question2 != 'Да' and question2 != 'да' and question2 != 'ДА' and question2 != 'Нет' and question2 != 'нет' and question != 'НЕТ':
             print('Попробуйте ввести еще раз')
             question2 = input('Хотите продолжить? ')
-        if question2 == 'Да' or question2 == 'да':
+        if question2 == 'Да' or question2 == 'да' or question2 == 'ДА':
             continue
-        elif question2 == 'Нет' or question2 == 'нет':
+        elif question2 == 'Нет' or question2 == 'нет' or question2 == 'НЕТ':
             break
 print('До свидания!')
